0fe00f1d80082a01f0a10fd350fb4b7c282ccfa3
iamarasekera <iamarasekera.dev@gmail.com>
2024-04-18 17:30:25 +0800

    Added a button to navigate to regsitation flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-
+import Link from 'next/link';
 
 export default function Home() {
   return (
-<></>
+    <div className="flex justify-center items-center h-screen">
+    <Link href="/registration">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Go to Registration
+      </button>
+    </Link>
+  </div>
   );
 }
