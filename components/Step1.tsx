7b62b9a436040ccfb1da5c394b76469d3af53fc2
iamarasekera <iamarasekera.dev@gmail.com>
2024-04-18 17:41:58 +0800

    Updated styles in step 1

--- components/Step1.tsx
+++ components/Step1.tsx
@@ -29,10 +29,11 @@ const Step1: React.FC<Step1Props> = ({ onNext, onChange }) => {
 
   return (
     <div className="flex flex-col gap-4">
+      <p className="text-lg font-semibold">Step 1:</p>
       {/* Form inputs for email, password, and re-enter password */}
-      <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} className="border border-black rounded outline-none px-4 py-2"/>
-      <input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} className="border border-black rounded outline-none px-4 py-2" />
-      <input type="password" name="reEnterPassword" placeholder="Re-enter Password" value={formData.reEnterPassword} onChange={handleChange} className="border border-black rounded outline-none px-4 py-2"/>
+      <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} className="border border-black rounded outline-none px-4 py-2" style={{ color: 'black' }} />
+      <input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} className="border border-black rounded outline-none px-4 py-2" style={{ color: 'black' }} />
+      <input type="password" name="reEnterPassword" placeholder="Re-enter Password" value={formData.reEnterPassword} onChange={handleChange} className="border border-black rounded outline-none px-4 py-2" style={{ color: 'black' }} />
        {/* Button to proceed to the next step in the registration flow */}
       <button onClick={onNext} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Get Started</button>
     </div>
