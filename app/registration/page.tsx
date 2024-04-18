b733c86755f48ce682c49c04a9df84fe829543d2
iamarasekera <iamarasekera.dev@gmail.com>
2024-04-18 16:18:17 +0800

    Added registration page and removd default nexj Js code from index page

---
+++ app/registration/page.tsx
@@ -0,0 +1,14 @@
+// pages/RegistrationPage.tsx
+"use client"
+import React from 'react';
+import RegistrationFlow from '@/components/RegistrationFlow';
+
+const RegistrationPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <RegistrationFlow />
+    </div>
+  );
+};
+
+export default RegistrationPage;
